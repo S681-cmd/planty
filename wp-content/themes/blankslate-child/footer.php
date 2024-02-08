5b9465c4df2b7f14ff1fd0bddcc0ab9d19c5ee75
Sebastien Dzierlatka <sebastiendzierlatka@gmail.com>
2024-02-08 17:18:02 +0100

    mise en place page acceuil et rencontrer

--- wp-content/themes/blankslate-child/footer.php
+++ wp-content/themes/blankslate-child/footer.php
@@ -1,9 +1,9 @@
 </main>
-<?php get_sidebar(); ?>
+
 </div>
 <footer id="footer" role="contentinfo">
 <div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+    <a href=""> mentions légales </a>
 </div>
 </footer>
 </div>
